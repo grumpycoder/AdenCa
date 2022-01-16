fe8d0f6366bd2bdc6e957bbe78f586eed2bda466
Mark Lawrence <grumpycoder@outlook.com>
2022-01-16 09:13:50 -0600

    added dbcontext extension

--- src/Aden.Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Aden.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -5,24 +5,29 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Aden.Infrastructure.Persistence;
 
-public class ApplicationDbContext: DbContext
+public class ApplicationDbContext : DbContext
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
-    
+
     public DbSet<Specification> FileSpecifications { get; set; }
     public DbSet<Submission> Submissions { get; set; }
-    
+
     protected override void OnModelCreating(ModelBuilder builder)
     {
         builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
-        
-        // builder.Entity<Submission>()
-        //     .HasOne<Specification>(e => e.Specification)
-        //     .WithMany(g => g.Submissions)
-        //     .HasForeignKey("FileSpecificationId");
-        
     }
-    
+}
+
+public static class ApplicationDbContextExtensions
+{
+    public static async Task<Specification> SpecificationWithSubmissions(this ApplicationDbContext context,
+        int specificationId)
+    {
+        return await context.FileSpecifications
+            .Include(s => s.Submissions)
+            .FirstOrDefaultAsync(x => x.Id == specificationId)
+            ;
+    }
 }
